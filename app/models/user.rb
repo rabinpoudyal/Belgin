22e35a5baa4da11c5c0a49ce35bc26a7058605a0
rabinpoudyal1995 <rabinpoudyal1995@gmail.com>
2018-06-08 19:57:12 +0545

    Devise error messages shop show page sidenav color user registration validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,12 @@ class User < ApplicationRecord
   validates :terms_and_conditions, :acceptance => true
   has_one :shop
 
+  validates :username, :presence => true, :length => {:minimum => 3, :maximum => 30}
+  validates :first_name, :presence => true, :length => {:minimum => 3, :maximum => 30}
+  validates :last_name, :presence => true, :length => {:minimum => 3, :maximum => 30}
+  validates :country, :presence => true, :length => {:minimum => 3, :maximum => 30}
+  validates :address, :presence => true, :length => {:minimum => 3, :maximum => 30}
+  validates :city, :presence => true, :length => {:minimum => 3, :maximum => 30}
+
 
 end
